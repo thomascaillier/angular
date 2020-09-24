1f019161322a3a341bab41b68f1a921d5567386d
Jacques Buchaillot <jacques.buchaillot@open-groupe.com>
2020-09-24 10:47:44 +0200

    Retirer Jasmine et Karma - Ajouter Jest

--- gerer-mes-taches/src/app/app.component.spec.ts
+++ gerer-mes-taches/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'gerer-mes-taches'`, () => {
+  it(`should have as title 'Formation Angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('gerer-mes-taches');
+    expect(app.title).toEqual('Formation Angular');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to gerer-mes-taches!');
+    expect(compiled.querySelector('h1').textContent).toContain(' Formation Angular ');
   });
 });
